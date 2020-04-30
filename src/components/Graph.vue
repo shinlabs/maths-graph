675d35f60c17f0253eb5e55a0609711bd92de4cf
Matatini Reia <shin_taku@hotmail.com>
2020-04-30 11:55:27 +0200

    001: display graph of 1/x function

--- src/components/Graph.vue
+++ src/components/Graph.vue
@@ -1,17 +1,59 @@
 <template>
   <div class="graph-wc-container">
     <h1>This will be a graph web component</h1>
-    <p>Type of function (continuous / discrete) : </p>
+    <p>Type of function (line / other) : </p>
     <p>{{ functionType }}</p>
+    <LineChart :chartConfig=planetChart></LineChart>
   </div>
 </template>
 
 <script lang="ts">
 import { Component, Prop, Vue } from 'vue-property-decorator'
+import { ChartConfiguration } from 'chart.js'
+import LineChart from '@/components/charts/LineChart.vue'
 
-@Component
+@Component({
+  components: { LineChart }
+})
 export default class Graph extends Vue {
   @Prop() private functionType!: string;
+
+  xCoordinates = [
+    // negative values
+    -10, -9, -8, -7, -6, -5, -4, -3, -2, -1,
+    -1 / 2, -1 / 3, -1 / 4, -1 / 5, -1 / 6, -1 / 7, -1 / 8, -1 / 9, -1 / 10,
+    0,
+    // positive values
+    1 / 10, 1 / 9, 1 / 8, 1 / 7, 1 / 6, 1 / 5, 1 / 4, 1 / 3, 1 / 2,
+    1, 2, 3, 4, 5, 6, 7, 8, 9, 10
+  ];
+
+  planetChart: ChartConfiguration = {
+    type: this.functionType,
+    data: {
+      datasets: [
+        {
+          label: 'f(x) = 1/x',
+          data: this.xCoordinates.map(x => { return { x, y: 1 / x } }),
+          borderColor: new Array(39).fill('#36495d'),
+          borderWidth: 3,
+          fill: false
+        }
+      ]
+    },
+    options: {
+      responsive: true,
+      scales: {
+        xAxes: [{
+          type: 'linear',
+          ticks: {
+            suggestedMin: -10,
+            suggestedMax: 10
+          }
+        }]
+      }
+    }
+  }
 }
 </script>
 
---
+++ src/components/charts/LineChart.vue
@@ -0,0 +1,24 @@
+<script lang="ts">
+import { Line } from 'vue-chartjs'
+import { Component, Prop, Vue } from 'vue-property-decorator'
+import { ChartData, ChartOptions, ChartConfiguration } from 'chart.js'
+
+@Component({
+  extends: Line
+})
+export default class LineChart extends Vue {
+  @Prop() chartConfig!: ChartConfiguration;
+
+  mounted () {
+    if (this.chartConfig.data && this.chartConfig.options) {
+      this.renderChart(this.chartConfig.data, this.chartConfig.options)
+    }
+  }
+
+  /**
+   * Little hack because there are issues in `vue-chartjs` package with TypeScript projects
+   * See https://github.com/apertureless/vue-chartjs/issues/313
+   */
+  renderChart!: (chartData: ChartData, options: ChartOptions) => void;
+}
+</script>
